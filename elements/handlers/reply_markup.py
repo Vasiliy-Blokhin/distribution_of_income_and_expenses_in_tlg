c7d1df2860399050299f7039ccddbe3bd88058ab
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2025-02-08 18:40:40 +1000

    update date block

--- elements/handlers/reply_markup.py
+++ elements/handlers/reply_markup.py
@@ -64,6 +64,8 @@ async def input(message: types.Message):
 async def today_date(callback: types.CallbackQuery, state: FSMContext):
     await state.set_state(InputData.date)
     await state.update_data(date='today')
+    await input_kind(state)
+
 
 @router.callback_query(F.data == 'дата')
 async def create_date(callback: types.CallbackQuery, state: FSMContext):
@@ -78,12 +80,14 @@ async def input_date(message: types.Message, state: FSMContext):
     await state.set_state(InputData.kind)
     await message.answer('input kind:')
 
+
 @router.message(InputData.kind)
 async def input_kind(message: types.Message, state: FSMContext):
     await state.update_data(kind=message.text)
     # add date validator
     await state.set_state(InputData.category)
     await message.answer('input category:')
+
 
 @router.message(InputData.category)
 async def input_category(message: types.Message, state: FSMContext):
@@ -92,14 +96,16 @@ async def input_category(message: types.Message, state: FSMContext):
     await state.set_state(InputData.value)
     await message.answer('input value:')
 
+
 @router.message(InputData.value)
 async def input_value(message: types.Message, state: FSMContext):
     await state.update_data(value=message.text)
     # add date validator
 
-
     data = await state.get_data()
     await message.answer(f'{data["date"]} - {data["kind"]} - {data["category"]} - {data["value"]}')
+
+
 # ________________________________________________________________
 @router.message(F.text.lower() == '/вывод')
 async def output(message: types.Message):
