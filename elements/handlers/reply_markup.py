83ee2d2faf6942f34b38c852b2b779514a769260
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2025-02-08 20:12:49 +1000

    update date block

--- elements/handlers/reply_markup.py
+++ elements/handlers/reply_markup.py
@@ -100,9 +100,9 @@ async def input_kind(message: types.Message, state: FSMContext):
     )
     await message.answer('Выберите kind: ', reply_markup=builder.as_markup())
 
-@router.callback_query(F.data.split(SPLIT_SYM)[0] == 'kind')
+@router.callback_query(F.text.split(SPLIT_SYM)[0] == 'kind')
 async def better_shares_result(callback: types.CallbackQuery, state: FSMContext):
-    await state.update_data(kind=F.data(SPLIT_SYM)[1])
+    await state.update_data(kind=F.text.split(SPLIT_SYM)[1])
     # add date validator
     await state.set_state(InputData.category)
     await callback.message.answer('input category:')
