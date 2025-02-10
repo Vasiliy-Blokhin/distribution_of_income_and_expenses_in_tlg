f5fc98763b9a90547398d3c630fe53d458d6dd37
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2025-02-10 20:37:05 +1000

    update output mode

--- elements/output/handler.py
+++ elements/output/handler.py
@@ -84,7 +84,8 @@ async def dates_variator(callback: types.CallbackQuery, state: FSMContext):
 
 @output_router.message(OutputData.date_start)
 async def different_years_and_dates(callback: types.CallbackQuery, state: FSMContext):
-    if await state.get_data()['flag'] == 1:
+    flag = await state.get_data()
+    if flag['flag'] == 1:
         start_date = '01.01.' + callback.message.text
         await state.update_data(date_start=start_date)
         end_date = '31.12.' + callback.message.text
@@ -95,7 +96,7 @@ async def different_years_and_dates(callback: types.CallbackQuery, state: FSMCon
             'Введите тип операций:',
             reply_markup=output_kind_builder().as_markup()
         )
-    elif await state.get_data()['flag'] == 2:
+    elif flag['flag'] == 2:
         await state.update_data(date_start=callback.message.text)
         await state.set_state(OutputData.date_end)
         await callback.message.answer('Введите конечную дату:')
