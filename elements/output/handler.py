5fb861bc9135258d28e24442e6c12e0005100908
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2025-02-12 21:02:03 +1000

    update reqruiments

--- elements/output/handler.py
+++ elements/output/handler.py
@@ -153,5 +153,5 @@ async def result(callback: types.CallbackQuery, state: FSMContext):
         f"-{request_data['date_end']}-{request_data['kind']}"
     )
     generate_xlsx(sorted_data=sorted_data, file_name=file_name)
-    file = open(file_name, 'rb')
+    file = open(f"{file_name}.xlsx", 'rb')
     callback.message.answer_document(document=InputFile(file))
