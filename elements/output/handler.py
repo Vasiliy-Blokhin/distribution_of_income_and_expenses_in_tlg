80ff90604dc34930823ac7791b81bd0bbb2a287f
Vasiliy-Blokhin <Vasiliyblo97@gmail.com>
2025-02-12 21:35:29 +1000

    update send document

--- elements/output/handler.py
+++ elements/output/handler.py
@@ -148,12 +148,11 @@ async def result(callback: types.CallbackQuery, state: FSMContext):
         f"-{request_data['date_end']}-{request_data['kind']}"
     )
     generate_xlsx(sorted_data=sorted_data, file_name=file_name)
-    await callback.message.answer_document(
-        document=FSInputFile(f"{file_name}.xlsx", file_name)
-    )
-    await callback.message.answer('file is send')
 
     await callback.message.answer(statistic_message(
         sorted_data,
         request_data
     ))
+    await callback.message.answer_document(
+        document=FSInputFile(f"{file_name}.xlsx")
+    )
